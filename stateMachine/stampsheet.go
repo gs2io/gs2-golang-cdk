4c923472e6c1c8bd35170c09aa0938d25963d8e8
gs2-ci <ci@gs2.io>
2023-07-27 13:45:11 +0000

    32b4792370a574f939e57372dd34ed44c6bad358

---
+++ stateMachine/stampsheet.go
@@ -0,0 +1,42 @@
+package stateMachine
+
+/*
+Copyright 2016 Game Server Services, Inc. or its affiliates. All Rights
+Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License").
+You may not use this file except in compliance with the License.
+A copy of the License is located at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+or in the "license" file accompanying this file. This file is distributed
+on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+express or implied. See the License for the specific language governing
+permissions and limitations under the License.
+*/
+
+import (
+	. "github.com/gs2io/gs2-golang-cdk/core"
+)
+
+var _ = AcquireAction{}
+
+func StartStateMachineByUserId(
+	namespaceName string,
+	args string,
+	ttl *int32,
+) AcquireAction {
+	properties := map[string]interface{}{
+		"userId": "#{userId}",
+	}
+	properties["namespaceName"] = namespaceName
+	properties["args"] = args
+	if ttl != nil {
+		properties["ttl"] = ttl
+	}
+	return AcquireAction{
+		Action:  "Gs2StateMachine:StartStateMachineByUserId",
+		Request: properties,
+	}
+}
