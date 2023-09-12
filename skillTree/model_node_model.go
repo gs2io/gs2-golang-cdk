49d555014c1d706a54b0b0d72122c458da42840d
gs2-ci <ci@gs2.io>
2023-09-12 02:40:49 +0000

    e143b97915f7b139f0b5a00c865e7e3e2d67352d

---
+++ skillTree/model_node_model.go
@@ -0,0 +1,80 @@
+package skillTree
+
+/*
+Copyright 2016 Game Server Services, Inc. or its affiliates. All Rights
+Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License").
+You may not use this file except in compliance with the License.
+A copy of the License is located at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+or in the "license" file accompanying this file. This file is distributed
+on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+express or implied. See the License for the specific language governing
+permissions and limitations under the License.
+*/
+
+import (
+	. "github.com/gs2io/gs2-golang-cdk/core"
+)
+
+var _ = AcquireAction{}
+
+type NodeModel struct {
+	Name                  string
+	Metadata              *string
+	ReleaseConsumeActions []ConsumeAction
+	ReturnAcquireActions  []AcquireAction
+	RestrainReturnRate    float32
+	PremiseNodeNames      []string
+}
+
+type NodeModelOptions struct {
+	Metadata             *string
+	ReturnAcquireActions []AcquireAction
+	PremiseNodeNames     []string
+}
+
+func NewNodeModel(
+	name string,
+	releaseConsumeActions []ConsumeAction,
+	restrainReturnRate float32,
+	options NodeModelOptions,
+) NodeModel {
+	data := NodeModel{
+		Name:                  name,
+		ReleaseConsumeActions: releaseConsumeActions,
+		RestrainReturnRate:    restrainReturnRate,
+		Metadata:              options.Metadata,
+		ReturnAcquireActions:  options.ReturnAcquireActions,
+		PremiseNodeNames:      options.PremiseNodeNames,
+	}
+	return data
+}
+
+func (p *NodeModel) Properties() map[string]interface{} {
+	properties := map[string]interface{}{}
+	properties["Name"] = p.Name
+	if p.Metadata != nil {
+		properties["Metadata"] = p.Metadata
+	}
+	{
+		values := make([]map[string]interface{}, len(p.ReleaseConsumeActions))
+		for i, element := range p.ReleaseConsumeActions {
+			values[i] = element.Properties()
+		}
+		properties["ReleaseConsumeActions"] = values
+	}
+	{
+		values := make([]map[string]interface{}, len(p.ReturnAcquireActions))
+		for i, element := range p.ReturnAcquireActions {
+			values[i] = element.Properties()
+		}
+		properties["ReturnAcquireActions"] = values
+	}
+	properties["RestrainReturnRate"] = p.RestrainReturnRate
+	properties["PremiseNodeNames"] = p.PremiseNodeNames
+	return properties
+}
---
+++ skillTree/ref_node_model.go
@@ -0,0 +1,49 @@
+package skillTree
+
+/*
+Copyright 2016 Game Server Services, Inc. or its affiliates. All Rights
+Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License").
+You may not use this file except in compliance with the License.
+A copy of the License is located at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+or in the "license" file accompanying this file. This file is distributed
+on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+express or implied. See the License for the specific language governing
+permissions and limitations under the License.
+*/
+
+import (
+	. "github.com/gs2io/gs2-golang-cdk/core"
+)
+
+var _ = AcquireAction{}
+
+type NodeModelRef struct {
+	NamespaceName string
+	NodeModelName string
+}
+
+func (p *NodeModelRef) Grn() string {
+	return NewJoin(
+		":",
+		[]string{
+			"grn",
+			"gs2",
+			NewGetAttrRegion().String(),
+			NewGetAttrOwnerId().String(),
+			"skillTree",
+			p.NamespaceName,
+			"model",
+			p.NodeModelName,
+		},
+	).String()
+}
+
+func (p *NodeModelRef) GrnPointer() *string {
+	grn := p.Grn()
+	return &grn
+}
