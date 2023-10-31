737effa240263b487e397c780aca54d563031b71
gs2-ci <ci@gs2.io>
2023-10-31 13:45:00 +0000

    78f5204dd420f7b7e7743df95cce10b432247656

---
+++ script/model_random_status.go
@@ -0,0 +1,56 @@
+package script
+
+/*
+Copyright 2016 Game Server Services, Inc. or its affiliates. All Rights
+Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License").
+You may not use this file except in compliance with the License.
+A copy of the License is located at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+or in the "license" file accompanying this file. This file is distributed
+on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+express or implied. See the License for the specific language governing
+permissions and limitations under the License.
+*/
+
+import (
+	. "github.com/gs2io/gs2-golang-cdk/core"
+)
+
+var _ = AcquireAction{}
+
+type RandomStatus struct {
+	Seed int64
+	Used []RandomUsed
+}
+
+type RandomStatusOptions struct {
+	Used []RandomUsed
+}
+
+func NewRandomStatus(
+	seed int64,
+	options RandomStatusOptions,
+) RandomStatus {
+	data := RandomStatus{
+		Seed: seed,
+		Used: options.Used,
+	}
+	return data
+}
+
+func (p *RandomStatus) Properties() map[string]interface{} {
+	properties := map[string]interface{}{}
+	properties["Seed"] = p.Seed
+	{
+		values := make([]map[string]interface{}, len(p.Used))
+		for i, element := range p.Used {
+			values[i] = element.Properties()
+		}
+		properties["Used"] = values
+	}
+	return properties
+}
