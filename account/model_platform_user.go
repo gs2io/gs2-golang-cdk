42d0bc61fb508a701b089d46077d5838ae4da0ac
gs2-ci <ci@gs2.io>
2024-07-10 03:41:32 +0000

    287bf32a66d5db194a08f2b1abc29fc6bd764e57

---
+++ account/model_platform_user.go
@@ -0,0 +1,54 @@
+package account
+
+/*
+Copyright 2016 Game Server Services, Inc. or its affiliates. All Rights
+Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License").
+You may not use this file except in compliance with the License.
+A copy of the License is located at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+or in the "license" file accompanying this file. This file is distributed
+on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+express or implied. See the License for the specific language governing
+permissions and limitations under the License.
+*/
+
+import (
+	. "github.com/gs2io/gs2-golang-cdk/core"
+)
+
+var _ = AcquireAction{}
+
+type PlatformUser struct {
+	Type           int32
+	UserIdentifier string
+	UserId         string
+}
+
+type PlatformUserOptions struct {
+}
+
+func NewPlatformUser(
+	Type int32,
+	userIdentifier string,
+	userId string,
+	options PlatformUserOptions,
+) PlatformUser {
+	data := PlatformUser{
+		Type:           Type,
+		UserIdentifier: userIdentifier,
+		UserId:         userId,
+	}
+	return data
+}
+
+func (p *PlatformUser) Properties() map[string]interface{} {
+	properties := map[string]interface{}{}
+	properties["Type"] = p.Type
+	properties["UserIdentifier"] = p.UserIdentifier
+	properties["UserId"] = p.UserId
+	return properties
+}
--- money2/ref_namespace.go
+++ money2/ref_namespace.go
@@ -14,6 +14,8 @@ or in the "license" file accompanying this file. This file is distributed
 on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
 express or implied. See the License for the specific language governing
 permissions and limitations under the License.
+
+deny overwrite
 */
 
 import (
@@ -37,7 +39,7 @@ func (p *NamespaceRef) StoreContentModel(
 
 func (p *NamespaceRef) VerifyReceipt(
 	contentName string,
-	receipt Receipt,
+	receipt string,
 ) ConsumeAction {
 	return VerifyReceiptByUserId(
 		p.NamespaceName,
